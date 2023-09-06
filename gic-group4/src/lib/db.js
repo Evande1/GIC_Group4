76935a896dd2ed953a8f036168479e41a06e033e
cadyli <cady.li@gmail.com>
2023-09-07 01:19:50 +0800

    added Docker file and connect to test lightsail application

---
+++ gic-group4/src/lib/db.js
@@ -0,0 +1,39 @@
+import mysql from "mysql2/promise";
+
+export async function query() {
+  const dbconfig = {
+    host: process.env.MYSQL_HOST,
+    port: 3306,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+  };
+  const pool = mysql.createPool(dbconfig);
+
+  try {
+    const connection = await pool.getConnection();
+    await connection.ping();
+    connection.release();
+    console.log("Database connection successful!");
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+}
+
+// export async function query({ query, values = [] }) {
+//   const dbconnection = await mysql.createConnection({
+//     host: process.env.MYSQL_HOST,
+//     port: 3306,
+//     database: process.env.MYSQL_DATABASE,
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//   });
+
+//   try {
+//     const [results] = await dbconnection.execute(query, values);
+//     dbconnection.end();
+//     return results;
+//   } catch (error) {
+//     throw Error(error.message);
+//   }
+// }

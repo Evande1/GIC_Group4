cbb781b670517f0bab8210948bd8a63d81585cfd
cadyli <cady.li@gmail.com>
2023-09-07 10:51:30 +0800

    github actions and connects to db on local

--- gic-group4/src/pages/api/hello.js
+++ gic-group4/src/pages/api/hello.js
@@ -2,16 +2,16 @@ import { query } from "@/lib/db";
 import { test } from "node:test";
 
 export default async function handler(req, res) {
-  try {
-    await query();
-  } catch (error) {
-    console.error("Error initializing app", error);
-  }
-  //   if (req.method == "GET") {
-  //     const test = await query({
-  //       query: "SELECT * FROM entities",
-  //       values: [],
-  //     });
-  //     res.status(200).json({ test: test });
+  //   try {
+  //     await query();
+  //   } catch (error) {
+  //     console.error("Error initializing app", error);
   //   }
+  if (req.method == "GET") {
+    const test = await query({
+      query: "SELECT * FROM entities",
+      values: [],
+    });
+    res.status(200).json({ test: test });
+  }
 }
